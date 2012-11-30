04c36422f6475e00ba63e9c2a091176b02640f5e
Naomi Dushay <ndushay@stanford.edu>
2012-11-30 14:35:07 -0800

    oai_harvest methods and tests works in progress

---
+++ lib/harvestdor/oai_harvest.rb
@@ -0,0 +1,113 @@
+require 'oai'
+
+module Harvestdor
+
+  # Mixin:  methods to perform an OAI harvest
+  class Client
+
+=begin
+    def find_by_sparql query, options = { :binding => 'pid' }
+      return to_enum(:find_by_sparql, query, options).to_a unless block_given?
+
+      self.sparql(query).each do |x|
+        obj = self.find(x[options[:binding]]) rescue nil
+        yield obj if obj
+      end
+    end
+=end
+  
+    # NAOMI_MUST_COMMENT_THIS_METHOD
+    # @return [Array<OAI::Record>] or enumeration over it, if block is given
+    def harvest_records options = {}
+      return to_enum(:harvest_records, options).to_a unless block_given?
+      
+      oai_options={}
+      oai_options[:metadata_prefix] = options[:metadata_prefix] ? options[:metadata_prefix] : config.default_metadata_prefix 
+      oai_options[:from] = options[:from] ? options[:from] : config.default_from_date
+      oai_options[:until] = options[:until] ? options[:until] : config.default_until_date
+      oai_options[:set] = options[:set] ? options[:set] : config.default_set
+
+      each_record(oai_options) do |oai_rec|
+        yield oai_rec
+      end
+    end
+  
+    # NAOMI_MUST_COMMENT_THIS_METHOD
+    # @return [Array<OAI::Header>] or enumeration over it, if block is given
+    def harvest_ids options = {}
+      return to_enum(:harvest_records, options).to_a unless block_given?
+      
+      oai_options={}
+      oai_options[:metadata_prefix] = options[:metadata_prefix] ? options[:metadata_prefix] : config.default_metadata_prefix 
+      oai_options[:from] = options[:from] ? options[:from] : config.default_from_date
+      oai_options[:until] = options[:until] ? options[:until] : config.default_until_date
+      oai_options[:set] = options[:set] ? options[:set] : config.default_set
+
+      each_header(oai_options) do |oai_hdr|
+        yield Harvestdor.druid(oai_hdr)
+      end
+    end
+    
+    # Iterate over the OAI client's records (following resumption tokens) and yield OAI::Record
+    # @return enumeration of [OAI::Record]
+    def each_record (oai_args, &block)
+      each_oai_object(true, oai_args, &block)
+    end
+    
+    # Iterate over the OAI client's headers (following resumption tokens) and yield OAI::Header
+    # @return enumeration of [OAI::Header]
+    def each_header (oai_args, &block)
+      each_oai_object(false, oai_args, &block)
+    end
+    
+    # Iterate over the OAI client's records (following resumption tokens) and yield OAI::Record
+    # @param [Boolean] harvest_records set to true to harvest OAI::Record objects; false for OAI::Header objects
+    # @return enumeration of [OAI::Record]
+    # TODO: This could be moved into ruby-oai?
+    def each_oai_object (harvest_records, oai_args, &block)
+      list_method_sym = :list_identifiers
+      if harvest_records
+        list_method_sym = :list_records
+      end
+      
+      response = oai_client.send list_method_sym, oai_args
+      while response.entries.size > 0
+        response.entries.each &block
+
+        token = response.resumption_token
+        if token.nil? or token.empty?
+          break
+        else
+          response = oai_client.send(list_method_sym, :resumption_token => token)
+        end
+      end
+    rescue Faraday::Error::TimeoutError => e
+      Harvestdor.logger.error "No response from OAI Provider"
+      Harvestdor.logger.error e
+    rescue OAI::Exception => e
+      # possibly unnecessary after ruby-oai 0.0.14
+      Harvestdor.logger.error "Received unexpected OAI::Exception"
+      Harvestdor.logger.error e
+    end
+    
+    
+    
+    # Are we harvesting a set that contains a collection object? 
+    def collection_harvest?
+      oai_options && oai_options[:set] && oai_options[:set].match(/is_member_of_collection/)
+    end
+  
+    # If the set we're harvesting includes a collection object, return the druid of the collection object.
+    # Otherwise, return nil. 
+    def collection_object_druid
+      if collection_harvest?
+        oai_options[:set].split('_').last
+      else
+        nil
+      end
+    end  
+  
+  end # class OaiHarvester
+
+end # module Harvestdor
+
---
+++ spec/oai_harvest_spec.rb
@@ -0,0 +1,175 @@
+require 'spec_helper'
+
+describe 'Harvestdor::Client oai harvesting' do
+  before(:all) do
+    @harvestdor_client = Harvestdor::Client.new
+  end
+  
+  describe "harvest_ids" do
+    it "should use default values for OAI arguments if they are not present in the method param hash" do
+      pending "to be implemented"
+    end
+    it "should use OAI arguments from the method param hash if they are present" do
+      pending "to be implemented"
+    end
+    it "should return a list of druids" do
+      pending "to be implemented"
+    end
+    it "should have results viewable as an array" do
+      pending "to be implemented"
+    end
+    it "should have enumerable results" do
+      pending "to be implemented"
+    end
+  end
+  
+  describe "harvest_records" do    
+    context "full harvest with resumption tokens" do
+      it "should stop processing when no records/headers are received" do
+        oai_response = mock('oai_response')
+        oai_response.stub(:entries).and_return([])
+        @harvestdor_client.oai_client.stub(:list_records).with(an_instance_of(Hash)) { 
+            oai_response
+        }
+
+        i = 0
+        @harvestdor_client.harvest_records { |record| i += 1 }
+        i.should == 0
+      end
+
+      it "should stop processing when the resumption token is empty" do
+        oai_response_with_token = mock('oai_response')
+        oai_response_with_token.stub(:entries).and_return([1,2,3,4,5])
+        oai_response_with_token.stub(:resumption_token).and_return('')
+
+        @harvestdor_client.oai_client.stub(:list_records).with(an_instance_of(Hash)) { 
+          oai_response_with_token
+        }
+
+        i = 0
+        @harvestdor_client.harvest_records { |record| i += 1 }
+        i.should == 5
+      end
+
+      it "should stop processing when there was no resumption token" do
+        oai_response_with_token = mock('oai_response')
+        oai_response_with_token.stub(:entries).and_return([1,2,3,4,5])
+        oai_response_with_token.stub(:resumption_token).and_return(nil)
+
+        @harvestdor_client.oai_client.stub(:list_records).with(an_instance_of(Hash)) { 
+          oai_response_with_token
+        }
+
+        i = 0
+        @harvestdor_client.harvest_records { |record| i += 1 }
+        i.should == 5
+      end      
+    end # resumption tokens
+
+  end
+  
+
+  describe "each_oai_object" do
+    it "should perform a list_records OAI request when first arg is true" do
+      oai_response = mock('oai_response')
+      oai_response.stub(:entries).and_return([])
+      @harvestdor_client.oai_client.stub(:list_records).with(an_instance_of(Hash)) { 
+          oai_response
+      }
+      @harvestdor_client.oai_client.should_receive(:list_records)
+      @harvestdor_client.each_oai_object(true, {})
+    end
+    
+    it "should perform a list_identifiers OAI request when first arg is false" do
+      oai_response = mock('oai_response')
+      oai_response.stub(:entries).and_return([])
+      @harvestdor_client.oai_client.stub(:list_identifiers).with(an_instance_of(Hash)) { 
+          oai_response
+      }
+      @harvestdor_client.oai_client.should_receive(:list_identifiers)
+      @harvestdor_client.each_oai_object(false, {})
+    end
+
+    it "should use passed OAI arguments" do
+      pending "to be implemented"
+    end
+    it "should yield to the passed block" do
+      pending "to be implemented"
+    end
+    
+    context "resumption tokens" do
+      it "should stop processing when no records/headers are received" do
+        oai_response = mock('oai_response')
+        oai_response.stub(:entries).and_return([])
+        @harvestdor_client.oai_client.stub(:list_records).with(an_instance_of(Hash)) { 
+            oai_response
+        }
+
+        i = 0
+        @harvestdor_client.each_oai_object(true, {}) { |record| i += 1 }
+        i.should == 0
+      end
+
+      it "should stop processing when the resumption token is empty" do
+        oai_response_with_token = mock('oai_response')
+        oai_response_with_token.stub(:entries).and_return([1,2,3,4,5])
+        oai_response_with_token.stub(:resumption_token).and_return('')
+        @harvestdor_client.oai_client.stub(:list_records).with(an_instance_of(Hash)) { 
+          oai_response_with_token
+        }
+
+        i = 0
+        @harvestdor_client.each_oai_object(true, {}) { |record| i += 1 }
+        i.should == 5
+      end
+
+      it "should stop processing when there was no resumption token" do
+        oai_response_with_token = mock('oai_response')
+        oai_response_with_token.stub(:entries).and_return([1,2,3,4,5])
+        oai_response_with_token.stub(:resumption_token).and_return(nil)
+        @harvestdor_client.oai_client.stub(:list_records).with(an_instance_of(Hash)) { 
+          oai_response_with_token
+        }
+
+        i = 0
+        @harvestdor_client.each_oai_object(true, {}) { |record| i += 1 }
+        i.should == 5
+      end      
+    end # resumption tokens
+
+  end
+  
+  describe "collection_harvest?" do
+    it "should do something" do
+      pending "to be implemented"
+    end
+  end
+  
+  context "#is_a_collection?" do
+    it "true if an OAI record is for a collection object" do
+      pending
+      @records[@batchelor_collection_druid.to_sym].solr_mapper.is_a_collection?.should eql(true)
+    end
+    it "true if an OAI header is for a collection object" do
+      pending
+    end
+    it "false if an OAI record is not a collection object" do
+      pending
+      @records[:bc497ws1916].solr_mapper.is_a_collection?.should eql(false)
+    end
+    it "false if an OAI header is not a collection object" do
+      pending
+    end
+  end
+
+  describe "collection_object_druid" do
+    it "should do something" do
+      pending "to be implemented"
+    end
+  end
+  
+  it "should keep utf-8 encoded characters intact" do
+    pending "to be implemented"
+  end
+    
+end
